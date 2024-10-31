351a2e8ce2b0ccf82627b0fa9363d722ca08dad6
hungyin <71914038+GZ-hongyin@users.noreply.github.com>
2024-10-31 23:51:24 +0800

    Update m.py

--- m.py
+++ m.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 classes = ['A','B','C','D','E']
 confusion_matrix = np.array([(9,1,3,4,0,1,1,1,1,1),(9,1,3,4,0,1,1,1,1,1),(9,1,3,4,0,1,1,1,1,1),(9,1,3,4,0,1,1,1,1,1),(9,1,3,4,0,1,1,1,1,1),(9,1,3,4,0,1,1,1,1,1),(9,1,3,4,0,1,1,1,1,1),(9,1,3,4,0,1,1,1,1,1),(9,1,3,4,0,1,1,1,1,1),(9,1,3,4,0,1,1,1,1,1)],dtype=np.float64)
 
-plt.imshow(confusion_matrix, interpolation='nearest', cmap=plt.cm.Oranges)  #按照像素显示出矩阵
+plt.imshow(confusion_matrix, interpolation='nearest', cmap=plt.cm.Oranges)  
 plt.title('confusion_matrix')
 plt.colorbar()
 tick_marks = np.arange(len(classes))
@@ -13,10 +13,10 @@ plt.yticks(tick_marks, classes)
 
 thresh = confusion_matrix.max() / 2.
 #iters = [[i,j] for i in range(len(classes)) for j in range((classes))]
-#ij配对，遍历矩阵迭代器
+
 iters = np.reshape([[[i,j] for j in range(10)] for i in range(10)],(confusion_matrix.size,2))
 for i, j in iters:
-    plt.text(j, i, format(confusion_matrix[i, j]),va='center',ha='center')   #显示对应的数字
+    plt.text(j, i, format(confusion_matrix[i, j]),va='center',ha='center') 
 
 plt.ylabel('Real label')
 plt.xlabel('Prediction')
